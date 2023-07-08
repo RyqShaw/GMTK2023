2c8efdade840b0eca1f7349886fa5fb1e0930347
Simber1842 <simber1842@gmail.com>
2023-07-08 17:25:20 -0500

    Machines chase hero

--- Modules/PlayerControls/MachineNumberLabel.gd
+++ Modules/PlayerControls/MachineNumberLabel.gd
@@ -11,6 +11,12 @@ func _ready() -> void:
 	GlobalInfo.tread_machine_num_changed.connect(update_label)
 
 func update_label() -> void:
+	if GlobalInfo.baseMachineNumber < 0:
+		GlobalInfo.baseMachineNumber = 0
+	if GlobalInfo.grabberMachineNumber < 0:
+		GlobalInfo.grabberMachineNumber = 0
+	if GlobalInfo.treadMachineNumber < 0:
+		GlobalInfo.treadMachineNumber = 0
 	var armors := "Armors: " + str(GlobalInfo.baseMachineNumber) + "/" + str(GlobalInfo.baseMachineLimit)
 	var grabbers := "Grabbers: " + str(GlobalInfo.grabberMachineNumber) + "/" + str(GlobalInfo.grabberMachineLimit)
 	var treads := "Treads: " + str(GlobalInfo.treadMachineNumber) + "/" + str(GlobalInfo.treadMachineLimit)
