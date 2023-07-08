64b0b6ae355a1fa4d15aad6401cbc15ec41583da
Sharyq Siddiqi <62477104+RyqShaw@users.noreply.github.com>
2023-07-08 09:59:38 -0400

    Power Label + Weapon Resource+ Hurt and Hit box

--- Modules/Hero/Hero.gd
+++ Modules/Hero/Hero.gd
@@ -1,9 +1,34 @@
 extends CharacterBody2D
 
+signal weapon_changed
+
 @export var hero_stats : HeroStats
+@export var weapon : HeroWeapon = preload("res://Modules/Hero/HeroWeapons/SwordWeapon.tres"):
+	get:
+		return weapon
+	set(value):
+		weapon = value
+		weapon_changed.emit()
 
 @export var movement_target : Node
 @onready var navigation_agent : NavigationAgent2D = $NavigationAgent2D
+@onready var hurtbox: Area2D = $Hurtbox
+
+func _ready() -> void:
+	self.connect("weapon_changed", set_attack_mode)
 
 func _physics_process(_delta: float) -> void:
 	pass
+
+func set_attack_mode() -> void:
+	pass
+
+func _on_hurtbox_area_entered(area: Area2D) -> void:
+	hero_stats.health -= area.damage
+	hurtbox.start_invincibility(0.6)
+
+func _on_hurtbox_invincibility_started() -> void:
+	pass # Replace with function body.
+
+func _on_hurtbox_invincibility_ended() -> void:
+	pass # Replace with function body.
---
+++ Resources/Stats/HeroWeapon.gd
@@ -0,0 +1,7 @@
+extends Resource
+class_name HeroWeapon
+
+@export var melee := true
+@export var projectile : PackedScene
+@export var damage : float = 10
+@export var knockback_scale : float = 1
