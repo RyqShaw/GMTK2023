73d8090b2418845ca00ceb4f7fa1e5c5a7b2f56f
Sharyq Siddiqi <62477104+RyqShaw@users.noreply.github.com>
2023-07-08 12:18:52 -0400

    Fixed Collision Layers

--- Modules/Machines/BaseMachine.gd
+++ Modules/Machines/BaseMachine.gd
@@ -9,12 +9,10 @@ func _ready() -> void:
 
 func in_valid_spawn() -> void:
 	if $InValidArea.has_overlapping_bodies() or $InValidArea.has_overlapping_areas() or cost > GlobalInfo.power:
-		print($InValidArea.get_overlapping_bodies())
 		queue_free()
-	else: 
+	else:
 		GlobalInfo.power -= cost
-		print(GlobalInfo.power)
-		print("valid")
+		$CollisionShape2D.disabled = false
 
 func _on_summoning_sickness_timeout() -> void:
 	in_valid_spawn()
